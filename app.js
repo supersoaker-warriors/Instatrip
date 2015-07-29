306893cf8a9b8937a1c0c72951a1297593398403
Brendan Dugan <dugan_brendan@yahoo.com>
2015-07-28 21:12:45 -0700

    [FIX] Fix Favicon Pathing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var apiRouter = require('./routes/apiRouter');
 var app = express();
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/Assets', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
