e0ffe3a80c80bf2328eb3ec0c4a6fe82569a8bed
Adi <avinnakotaster@gmail.com>
2015-07-30 22:05:48 -0700

    [MERGE] dealing with conflicts

--- public/js/instatrip.services.js
+++ public/js/instatrip.services.js
@@ -429,7 +429,6 @@ angular.module('instatrip.services', [])
 
   // Initiate Instagram request and package response into display
   var getPhoto = function(routes){
-    console.log(routes);
     var imgHolder = [];
     var linkHolder = {};
     return $http({
