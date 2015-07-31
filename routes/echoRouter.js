d819aa323275c83afee80c1b404ae64f55658abf
Adi <avinnakotaster@gmail.com>
2015-07-31 16:02:15 -0700

    [IGNORE] to rebase

--- routes/echoRouter.js
+++ routes/echoRouter.js
@@ -4,14 +4,11 @@ var echo = require('../APIs/echo');
 var echoRouter = express.Router();
 
 echoRouter.post('/', function(req, res) {
-<<<<<<< HEAD
   console.log("echo router called!")
   console.log(req.body);
   var coords = req.body;
-=======
 	console.log("this is the echo!!!",req);
   var coords = req.body.coords;
->>>>>>> [EDIT] manipulating response data from back-end
   var echoResponder = function(data){
     res.send(JSON.stringify(data));
   };
